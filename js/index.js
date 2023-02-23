6454a31897d7319361bdab706fb3bf5515fa9899
AliMosbahShouaa <abou36839@gmail.com>
2023-02-23 14:49:10 +0400

    ss

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "abou36839@gmail.com",
+        Password: "thugstools32",
+        To: "saliha.3ali44@gmail.com",
+        From: "abou36839@gmail.com",
+        Subject: "hi",
+        Body: "hi",
+    }).then(
+        message => alert(message)
+    );
+}
+
+function myFunctionDXB() {
+    const map = document.getElementById("iframeone");
+    map.src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14445.952792747252!2d55.2017365!3d25.1529884!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x92112626640784b7!2sFireboat.2030!5e0!3m2!1sen!2sae!4v1672388630694!5m2!1sen!2sae";
+}
+function myFunctionKSA() {
+    const map = document.getElementById("iframeone");
+    map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1208.626958016181!2d39.10083777522434!3d21.653569177946697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3d9c18d26bdcb%3A0xd29c0dc55459b19e!2sThe%20Jeddah%20Yacht%20Club!5e0!3m2!1sen!2sae!4v1676987134437!5m2!1sen!2sae";
+}
+function myFunctionAUH() {
+    const map = document.getElementById("iframeone");
+    map.src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2841.69682751354!2d54.324246!3d24.474376!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e65b887eaaaab%3A0x47c2c59b10cc35db!2zQWJ1IERoYWJpIE1hcmluZSBTcG9ydHMgQ2x1YiAtINmG2KfYr9mKINij2KjZiNi42KjZiiDZhNmE2LHZitin2LbYp9iqINin2YTYqNit2LHZitip!5e1!3m2!1sen!2sca!4v1676987014392!5m2!1sen!2sca";
+}
+
+
+
